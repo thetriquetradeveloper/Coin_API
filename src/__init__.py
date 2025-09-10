5eb1ccb0b28f9f1c491a2d8f474d6556bd46f792
Faizan code <faizansaleem0029@gmail.com>
2025-09-10 05:02:31 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,14 @@
-# This file is intentionally left blank.
+"""
+Coin API Package
+
+A Python package for getting cryptocurrency prices from CoinMarketCap.
+"""
+
+from .COIN_API import coin, main
+
+
+__version__ = "1.0.0"
+__author__ = "faizan code"
+__email__ = "thetriquetrathetriquetradeveloper@gmail.com"
+
+__all__ = ["coin", "main"]
